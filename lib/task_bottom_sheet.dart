6d218f975b3fea3911a1c718cfa3d78d41b43045
lola208 <beckori12@gmail.com>
2023-09-26 00:10:41 +0300

    fixed some errors

--- lib/task_bottom_sheet.dart
+++ lib/task_bottom_sheet.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do/providers/theme_provider.dart';
 import 'package:to_do/shared/styles/colors.dart';
 
 class TaskSheet extends StatefulWidget {
@@ -15,76 +17,99 @@ class _TaskSheetState extends State<TaskSheet> {
 
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<ThemeProvider>(context);
     TextEditingController taskController = TextEditingController();
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Text(
-            "Add new Task",
-            style: GoogleFonts.poppins(
-                textStyle:
-                    TextStyle(fontWeight: FontWeight.w700, fontSize: 18)),
-          ).tr(),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextField(
-            controller: taskController,
-            style: TextStyle(),
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: MyColors.appBarColor,
-                  ),
-                  borderRadius: BorderRadius.circular(12)),
-              hintText: "enter your task".tr(),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: Padding(
+    return Container(
+      color: provider.mode == ThemeMode.light
+          ? Colors.white
+          : MyColors.lighterBlack,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
             padding: const EdgeInsets.all(20.0),
             child: Text(
-              "Select Time",
-              textAlign: TextAlign.start,
+              "Add new Task",
               style: GoogleFonts.poppins(
-                  textStyle:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
+                  textStyle: TextStyle(
+                      color: provider.mode == ThemeMode.light
+                          ? Colors.black
+                          : Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18)),
             ).tr(),
           ),
-        ),
-        InkWell(
-            onTap: () {
-              selectDate();
-            },
-            child: Text(
-              "${selectedDate.toString().substring(0, 10)}",
-              style: TextStyle(color: MyColors.appBarColor, fontSize: 16),
-            )),
-        Padding(
-          padding: const EdgeInsets.only(top: 40.0, bottom: 40),
-          child: ElevatedButton(
-            onPressed: () {},
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Add Task",
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(fontSize: 18),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              controller: taskController,
+              style: TextStyle(
+                  color: provider.mode == ThemeMode.light
+                      ? Colors.black
+                      : Colors.white),
+              decoration: InputDecoration(
+                hintStyle: TextStyle(
+                    color: provider.mode == ThemeMode.light
+                        ? Colors.black
+                        : Colors.grey),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: MyColors.appBarColor,
+                    ),
+                    borderRadius: BorderRadius.circular(12)),
+                hintText: "enter your task".tr(),
               ),
             ),
-            style: ElevatedButton.styleFrom(
-                backgroundColor: MyColors.appBarColor,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12))),
           ),
-        )
-      ],
+          Align(
+            alignment:
+                EasyLocalization.of(context)!.currentLocale == Locale("en")
+                    ? Alignment.topLeft
+                    : Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Select Time",
+                style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                        color: provider.mode == ThemeMode.light
+                            ? Colors.black
+                            : Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400)),
+              ).tr(),
+            ),
+          ),
+          InkWell(
+              onTap: () {
+                selectDate();
+              },
+              child: Text(
+                "${selectedDate.toString().substring(0, 10)}",
+                style: TextStyle(color: MyColors.appBarColor, fontSize: 16),
+              )),
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0, bottom: 40),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Add Task".tr(),
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontSize: 18),
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: MyColors.appBarColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12))),
+            ),
+          )
+        ],
+      ),
     );
   }
 
